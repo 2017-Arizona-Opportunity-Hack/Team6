0ae91c9322598f7b8639dc491a1007ebb47e60cb
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2017-10-21 13:55:28 -0700

    Fix indentation

--- OHacksBackend/app/database/Foster.js
+++ OHacksBackend/app/database/Foster.js
@@ -5,25 +5,25 @@
 var mongoose = require('mongoose');
 
 var schema = mongoose.Schema({
-        Foster : {
-                main : {
-					email : String,
-					name : String,
-					password : String,
-                },
-				preferences : {
-					user_location : String,
-					time_needed_by : String,
-					type : String,
-					size : String,
-				},
-				dogFostered : {
-					dogInfo : {
-						id: String,
-						time_adopted : String,
-						time_until : String,
-					}
-				},
-        }});
+	Foster : {
+		main : {
+			email : String,
+			name : String,
+			password : String,
+		},
+		preferences : {
+			user_location : String,
+			time_needed_by : String,
+			type : String,
+			size : String,
+		},
+		dogFostered : {
+			dogInfo : {
+				id: String,
+				time_adopted : String,
+				time_until : String,
+			}
+		},
+	}});
 
 module.exports = mongoose.model('Foster', schema);
