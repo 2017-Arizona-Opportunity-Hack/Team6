2505e2a580dc38451702471f496ea2e9dbefa5cb
steven85048 <steven85048@gmail.com>
2017-10-21 12:56:56 -0700

    modified auth tokens for google+

--- OHacksBackend/app/passport/auth.js
+++ OHacksBackend/app/passport/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'googleAuth' : {
-		'clientId' : 'Client_ID',
-		'clientSecret' : 'Client_Secret',
-		'callbackURL' : 'https://damp-cove-80159.herokuapp.com/auth/google/callback',
+		'clientId' : '172816239656-jp772bvbot0r44l66qclfvk9eqhirim8.apps.googleusercontent.com',
+		'clientSecret' : '3EcEr4ppL1HNHEiiChoPHEG_',
+		'callbackURL' : 'localhost:8080/dogadd',
 	}
 } 
