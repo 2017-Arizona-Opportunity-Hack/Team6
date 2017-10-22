4c18cf9a07267b006ad174e45daeda976a1833d8
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2017-10-21 22:06:56 -0700

    FUCK RYAN

--- OHacksBackend/app/database/FosteredDog.js
+++ OHacksBackend/app/database/FosteredDog.js
@@ -5,16 +5,16 @@
 var mongoose = require('mongoose');
 
 var schema = mongoose.Schema({
-        FosteredDog : {
-      dogName : String,
-			time_needed_by : Number,
-			location : String,
-      species: String,
-			breed : String,
-			weight : Number,
-			owner_id : String,
-			vacc_date : Number,
-			vacc_info : String
-        }});
+	FosteredDog : {
+		dogName : String,
+		time_needed_by : Number,
+		location : String,
+		species: String,
+		breed : String,
+		weight : Number,
+		owner_id : String,
+		vacc_date : Number,
+		vacc_info : String
+	}});
 
 module.exports = mongoose.model('FosteredDog', schema);
